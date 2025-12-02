b4b887aa74fb74fbf727a04cca6c256a1345f9f1
Leo Gott <heptamaniac@gmail.com>
2025-12-02 20:13:55 +0000

    added multiplication, rotation and translation tests. Fixed translation issue

--- vmlib-test/rotation.cpp
+++ vmlib-test/rotation.cpp
@@ -1,3 +1,224 @@
 // You will need to define your own tests. Refer to CW1 or Exercise G.3 for
 // examples.
+#define _USE_MATH_DEFINES
+#include <catch2/catch_amalgamated.hpp>
 
+#include <numbers>
+#include <math.h>
+
+#include "../vmlib/mat44.hpp"
+
+TEST_CASE("Rotation-x", "[mat44]")
+{
+	static constexpr float kEps_ = 1e-4f;
+
+	using namespace Catch::Matchers;
+
+	SECTION("30 degree")
+	{
+		Mat44f expected = { {
+			1.000000f, 0.000000f, 0.000000f, 0.000000f,
+			0.000000f, 0.8660254f, -0.500000f, 0.000000f,
+			0.000000f, 0.500000f, 0.8660254f, 0.000000f,
+			0.000000f, 0.000000f, 0.000000f, 1.000000f
+		} };
+
+		Mat44f result = make_rotation_x(M_PI / 6); // 30-degree rotation
+		
+		//check result matches expected result
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+
+				REQUIRE_THAT((result[i, j]), WithinAbs(expected[i, j], kEps_));
+			}
+		}
+	}
+
+	SECTION("small rotation")
+	{
+		Mat44f expected = { {
+			1.000000f, 0.000000f, 0.000000f, 0.000000f,
+			0.000000f, 0.99950656f, -0.03141076f, 0.000000f,
+			0.000000f, 0.03141076f, 0.99950656f, 0.000000f,
+			0.000000f, 0.000000f, 0.000000f, 1.000000f
+		} };
+
+
+		Mat44f result = make_rotation_x(M_PI / 100); // 1.8-degree rotation
+
+		//check result matches expected result
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+
+				REQUIRE_THAT((result[i, j]), WithinAbs(expected[i, j], kEps_));
+			}
+		}
+	}
+
+	SECTION("negative rotation") 
+	{
+		Mat44f expected = { {
+			1.000000f, 0.000000f, 0.000000f, 0.000000f,
+			0.000000f, 0.70710678f, 0.70710678f, 0.000000f,
+			0.000000f, -0.70710678f, 0.70710678f, 0.000000f,
+			0.000000f, 0.000000f, 0.000000f, 1.000000f
+		} };
+
+
+		Mat44f result = make_rotation_x(- M_PI / 4); // -45-degree rotation
+
+		//check result matches expected result
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+
+				REQUIRE_THAT((result[i, j]), WithinAbs(expected[i, j], kEps_));
+			}
+		}
+	}
+
+}
+
+TEST_CASE("Rotation-y", "[mat44]")
+{
+	static constexpr float kEps_ = 1e-4f;
+
+	using namespace Catch::Matchers;
+
+	SECTION("30 degree")
+	{
+		Mat44f expected = { {
+			0.8660254f, 0.000000f, 0.500000f, 0.000000f,
+			0.000000f, 1.000000f, 0.000000f, 0.000000f,
+			-0.500000f, 0.000000f, 0.8660254f, 0.000000f,
+			0.000000f, 0.000000f, 0.000000f, 1.000000f
+
+		} };
+
+		Mat44f result = make_rotation_y(M_PI / 6); // 30-degree rotation
+
+		//check result matches expected result
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+
+				REQUIRE_THAT((result[i, j]), WithinAbs(expected[i, j], kEps_));
+			}
+		}
+	}
+
+	SECTION("small rotation")
+	{
+		Mat44f expected = { {
+			0.99950656f, 0.000000f, 0.03141076f, 0.000000f,
+			0.000000f, 1.000000f, 0.000000f, 0.000000f,
+			-0.03141076f, 0.000000f, 0.99950656f, 0.000000f,
+			0.000000f, 0.000000f, 0.000000f, 1.000000f
+
+		} };
+
+
+		Mat44f result = make_rotation_y(M_PI / 100); // 1.8-degree rotation
+
+		//check result matches expected result
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+
+				REQUIRE_THAT((result[i, j]), WithinAbs(expected[i, j], kEps_));
+			}
+		}
+	}
+
+	SECTION("negative rotation")
+	{
+		Mat44f expected = { {
+			0.70710678f, 0.000000f, -0.70710678f, 0.000000f,
+			0.000000f, 1.000000f, 0.000000f, 0.000000f,
+			0.70710678f, 0.000000f, 0.70710678f, 0.000000f,
+			0.000000f, 0.000000f, 0.000000f, 1.000000f
+
+		} };
+
+
+		Mat44f result = make_rotation_y(-M_PI / 4); // -45-degree rotation
+
+		//check result matches expected result
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+
+				REQUIRE_THAT((result[i, j]), WithinAbs(expected[i, j], kEps_));
+			}
+		}
+	}
+
+}
+
+TEST_CASE("Rotation-z", "[mat44]")
+{
+	static constexpr float kEps_ = 1e-4f;
+
+	using namespace Catch::Matchers;
+
+	SECTION("30 degree")
+	{
+		Mat44f expected = { {
+			0.8660254f, -0.500000f, 0.000000f, 0.000000f,
+			0.500000f, 0.8660254f, 0.000000f, 0.000000f,
+			0.000000f, 0.000000f, 1.000000f, 0.000000f,
+			0.000000f, 0.000000f, 0.000000f, 1.000000f
+
+		} };
+
+		Mat44f result = make_rotation_z(M_PI / 6); // 30-degree rotation
+
+		//check result matches expected result
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+
+				REQUIRE_THAT((result[i, j]), WithinAbs(expected[i, j], kEps_));
+			}
+		}
+	}
+
+	SECTION("small rotation")
+	{
+		Mat44f expected = { {
+			0.99950656f, -0.03141076f, 0.000000f, 0.000000f,
+			0.03141076f, 0.99950656f, 0.000000f, 0.000000f,
+			0.000000f, 0.000000f, 1.000000f, 0.000000f,
+			0.000000f, 0.000000f, 0.000000f, 1.000000f
+
+		} };
+
+
+		Mat44f result = make_rotation_z(M_PI / 100); // 1.8-degree rotation
+
+		//check result matches expected result
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+
+				REQUIRE_THAT((result[i, j]), WithinAbs(expected[i, j], kEps_));
+			}
+		}
+	}
+
+	SECTION("negative rotation")
+	{
+		Mat44f expected = { {
+			0.70710678f, 0.70710678f, 0.000000f, 0.000000f,
+			-0.70710678f, 0.70710678f, 0.000000f, 0.000000f,
+			0.000000f, 0.000000f, 1.000000f, 0.000000f,
+			0.000000f, 0.000000f, 0.000000f, 1.000000f
+		} };
+
+
+		Mat44f result = make_rotation_z(-M_PI / 4); // -45-degree rotation
+
+		//check result matches expected result
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+
+				REQUIRE_THAT((result[i, j]), WithinAbs(expected[i, j], kEps_));
+			}
+		}
+	}
+
+}
--- vmlib-test/translation.cpp
+++ vmlib-test/translation.cpp
@@ -1,3 +1,55 @@
 // You will need to define your own tests. Refer to CW1 or Exercise G.3 for
 // examples.
 
+#include <catch2/catch_amalgamated.hpp>
+
+#include <numbers>
+
+#include "../vmlib/mat44.hpp"
+
+TEST_CASE("translation", "[mat44]")
+{
+	static constexpr float kEps_ = 1e-4f;
+
+	using namespace Catch::Matchers;
+
+	SECTION("diagonal")
+	{
+		Mat44f test_m = { {
+			1.f, 0.f, 0.f, 1.f,
+			0.f, 1.f, 0.f, 1.f,
+			0.f, 0.f, 1.f, 1.f,
+			0.f, 0.f, 0.f, 1.f
+		} };
+
+		Mat44f result = make_translation({ 1.f, 1.f, 1.f });
+
+		//ensure no change has been made to test_m
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+
+				REQUIRE_THAT((result[i, j]), WithinAbs(test_m[i, j], kEps_));
+			}
+		}
+	}
+
+	SECTION("small")
+	{
+		Mat44f test_m = { {
+			1.f, 0.f, 0.f, 0.000001f,
+			0.f, 1.f, 0.f, 0.000001f,
+			0.f, 0.f, 1.f, 0.000001f,
+			0.f, 0.f, 0.f, 1.f
+		} };
+
+		Mat44f result = make_translation({ 0.000001f, 0.000001f, 0.000001f });
+
+		//ensure no change has been made to test_m
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+
+				REQUIRE_THAT((result[i, j]), WithinAbs(test_m[i, j], kEps_));
+			}
+		}
+	}
+}
